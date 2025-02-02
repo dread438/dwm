e41c690b261d28f51698d7cdf07b3b9ce0bd7677
Dread <dread438@proton.me>
2025-02-02 20:57:51 +0200

    Use custom dmenu_run

--- config.def.h
+++ config.def.h
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray2, "-sb", col_selectedtext, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenuspawn", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray2, "-sb", col_selectedtext, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
