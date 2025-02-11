aadaba616e2ce67d13e1e8a69cc4c1b0948a9e44
Dread <dread438@proton.me>
2025-02-11 22:43:24 +0200

    Update colorscheme

--- config.def.h
+++ config.def.h
@@ -9,18 +9,18 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
-static const char col_gray1[]       = "#080808";
-static const char col_gray2[]       = "#888888";
+static const char col_gray1[]       = "#111111";
+static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_gray5[]       = "#444444";
-static const char col_gray6[]       = "#222222";
 static const char col_cyan[]        = "#005577";
-static const char col_selectedtext[]= "#ffffff";
+static const char col_blue1[]		= "#80a0ff";
+static const char col_blue2[]		= "#74b2ff";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray2, col_gray1, col_gray5 },
-	[SchemeSel]  = { col_selectedtext, col_gray1,  col_gray2  },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray3 },
+	[SchemeSel]  = { col_gray1, col_blue1,  col_blue1  },
 };
 
 /* tagging */
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenuspawn", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray2, "-sb", col_selectedtext, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_blue1, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
